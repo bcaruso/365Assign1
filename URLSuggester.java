ad128fa548f838e0950fd87a2f14c6cc68af823b
Brandon Caruso <bcaruso@oswego.edu>
2015-02-07 12:52:06 -0500

    Created Main Class.

---
+++ URLSuggester.java
@@ -0,0 +1,19 @@
+/*
+*	URLSuggester CLASS - CSC 365 - Assignment 1
+*
+*	Author: Brandon Caruso
+*	Main Class which will handle:
+*		- GUI Setup		
+*		- Histogram Setup
+*		- URL Suggesting
+*/
+
+public class URLSuggester {
+
+		private Histogram[] urlHistograms;
+	
+		public static void main(String[] args){
+		
+		}	
+		
+}
