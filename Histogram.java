d49e08a59ea7eb75f4dc630a2e8862bd8bba1d07
Brandon Caruso <bcaruso@oswego.edu>
2015-02-07 12:29:23 -0500

    Crafted interiors of class Histogram. Method signatures created for intial methods that came to mind and consturtor defined (overloaded).

--- Histogram.java
+++ Histogram.java
@@ -9,4 +9,39 @@
 public class Histogram {
 
 
+	private float loadFactor = .75;
+	private int numberOfEntries = 0;
+	private Entry[] histogram;
+	
+	public Histogram(int size){
+		histogram = new Entry[size];
+	}
+	
+	public Histogram(int size, float loadF ){
+		histogram = new Entry[size];
+		loadFactor = loadF;
+	}
+	
+	public int getCount(String key){
+			return 0;
+	}
+	
+	public void add(String key){
+	
+	}
+	
+	private void rehash(){
+		
+	}
+	
+	public Entry contains(String key){
+			return null;
+	}
+
+	/*  
+	*	SOME WAY TO ITERATE THROUGH THE HISTOGRAM
+	* 	TODO: DECIDED BETWEEN A forEach OR Iterator
+	*/
+	
+	
 }
