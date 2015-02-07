daf4c96ac880842a1c889399388bed8ccf404ecf
Brandon Caruso <bcaruso@oswego.edu>
2015-02-07 12:05:28 -0500

    Crafted interiors of class Entry. Defined access methods and constructor.

--- Entry.java
+++ Entry.java
@@ -7,6 +7,37 @@
 */
 
 public class Entry {
+	
+	private int hash;
+	private String key;	// KEY
+	private int count;	// VALUE
+	private Entry next;
 
 
+	public Entry (String k) {
+		key = k.lowerCase();
+		count = 0;
+		next = null;
+		hash = key.hashCode();	
+	}
+
+	public int getHashCode(){
+		// Returns Hash Code for this specific Key
+		return hash;
+	}
+	
+	public int getCount(){
+		// Returns number of occurrences of this specific Key
+		return count;
+	}
+	
+	public String getKey(){
+		// Returns key
+		return key;
+	}
+	
+	public Entry next(){
+		// Returns next entry
+		return next;
+	}
 }
