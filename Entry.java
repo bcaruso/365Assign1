0c93fb49a09c54b1156dccd6123ab976851215ac
Brandon Caruso <bcaruso@oswego.edu>
2015-02-07 11:51:15 -0500

    Created two classes: Histogram and Entry. Histogram to define the main map and the Entry class to define the Entries for that map.

---
+++ Entry.java
@@ -0,0 +1,12 @@
+/*
+*	ENTRY CLASS - CSC 365 - Assignment 1
+*
+*	Author: Brandon Caruso
+*	Key: a string lowercase ( i.e. "the" )
+*	Value: a count
+*/
+
+public class Entry {
+
+
+}
---
+++ Histogram.java
@@ -0,0 +1,12 @@
+/*
+*	HISTOGRAM CLASS - CSC 365 - Assignment 1
+*
+*	Author: Brandon Caruso
+*	A hash map containing Strings and the Frequency of that String
+*	
+*/
+
+public class Histogram {
+
+
+}
