2440d8d6f2a85d142a6e5058fd8f4de0ec2a6a6b
Brandon Caruso <bcaruso@oswego.edu>
2015-02-14 13:20:37 -0500

    Class that creates a Histogram object from a URL.

--- URLHandler.java
+++ URLHandler.java
@@ -5,8 +5,37 @@
 *   Handles creating Histograms from URLs.
 */
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import java.net.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import java.util.Scanner;
+
 public class URLHandler {
 
-		
+		public static Histogram createHistogram(URL url) throws IOException, URISyntaxException{
+			
+			Histogram h = new Histogram(128);
+			
+			Document doc = Jsoup.parse(url.openStream(),null,url.toURI().toString());
+			
+			String dirtyDoc = doc.text();
+			//Remove Punctuation
+			String cleanDoc = dirtyDoc.replaceAll("[^a-zA-Z0-9 ]", "");
+			System.out.println(cleanDoc);
+			// Create Histogram
+			
+			Scanner s = new Scanner(cleanDoc);
+			
+			while(s.hasNext())
+				h.add(s.next());
+				
+			s.close();
+			
+			return h;
+			
+		}
 		
 }
