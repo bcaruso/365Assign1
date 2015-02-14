0e6159389efab4d6362db771fa949d7a5acc15fc
Brandon Caruso <bcaruso@oswego.edu>
2015-02-14 13:23:02 -0500

    Takes filename of initial URLs as a startup argument then createHistograms for each URL in the file.

--- URLSuggester.java
+++ URLSuggester.java
@@ -9,23 +9,48 @@
 */
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
 
 public class URLSuggester {
 
+		public static ArrayList<Histogram> urlHistograms;
+		public static Histogram queryHistogram;
 	
-		public static void main(String[] args){
+	
+		public static void main(String[] args) throws IOException, URISyntaxException{
 			
-			Histogram[] urlHistograms;
+			urlHistograms = new ArrayList<Histogram>();
 			
-			/* Initialize gui */		
+			// Get initURLs File 
+			File initURLs = new File(args[0]);
+			// Parse and create Histogram for each URL in File
+			
+			Scanner s = new Scanner(initURLs);
+			while(s.hasNext()){
+				URL url = new URL(s.nextLine());
+				System.out.println(url.toString());
+				urlHistograms.add(URLHandler.createHistogram(url));
+				System.out.println(urlHistograms.size());
+			}
+			
+			s.close();
+			
+			/*
+			// Initialize gui		
 			URLSuggesterGUI gui = new URLSuggesterGUI();
 			
-			/* Establish Event Dispatch Thread and Create GUI */
+			// Establish Event Dispatch Thread and Create GUI
 			SwingUtilities.invokeLater(new Runnable(){
 				public void run(){
 					gui.createGUI();
 				}
 			});
+			*/
 			
 		}	
 		
