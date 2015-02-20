ed4def9728419793cad3eb8c8e72331ceca1534d
Brandon Caruso <bcaruso@oswego.edu>
2015-02-20 11:42:36 -0500

    Now holds a reference to it's corresponding URL, Rehash Implemented, forEach Method Implement, and reduce Method Implement

--- Histogram.java
+++ Histogram.java
@@ -6,22 +6,30 @@
 *	
 */
 
+import java.util.function.Consumer;
+import java.util.function.BiFunction;
+import java.net.*;
+
 public class Histogram {
 
 
 	private double loadFactor = .75;
 	private int numberOfEntries = 0;
+	private URL url;
 	private Entry[] histogram;
 	
-	public Histogram(){
+	public Histogram(URL u){
+		url = u;
 		histogram = new Entry[16];
 	}
 	
-	public Histogram(int size){
+	public Histogram(URL u, int size){
+		url = u;
 		histogram = new Entry[size];
 	}
 	
-	public Histogram(int size, float loadF ){
+	public Histogram(URL u, int size, float loadF ){
+		url = u;
 		histogram = new Entry[size];
 		loadFactor = loadF;
 	}
@@ -46,13 +54,7 @@ public class Histogram {
 		
 		if(contains == null){
 			// NOPE!
-			Entry e = new Entry(key);
-			int eHash = e.getHashCode();
-			int index = Math.abs(eHash) % histogram.length;
-			Entry current = histogram[index];
-			e.next = current;
-			histogram[index] = e;
-			numberOfEntries += 1;
+			addEntry(new Entry(key));
 		}else{
 			//YEP!
 			contains.incCount();
@@ -64,17 +66,36 @@ public class Histogram {
 		}	
 	}
 	
-	private void rehash(){
-		System.out.println("Rehashing...");
-		
-		// A new Histogram
-		Entry[] newArray = new Entry[histogram.length * 2];
-		numberOfEntries = 0;
-		// For each element in current histogram add to newHistogram
-		// newHistogram.add();
-		
-		histogram = newArray;
+	private void addEntry(Entry e){
+		int eHash = e.getHashCode();
+		int index = Math.abs(eHash) % histogram.length;
+		Entry current = histogram[index];
+		e.next = current;
+		histogram[index] = e;
+		numberOfEntries += 1;
 	}
+	
+	private void rehash(){
+		//System.out.println("Rehashing...");
+		// A new Histogram
+		Histogram newHistogram = new Histogram(url,histogram.length * 2);
+		
+		// Copy the Entries over!
+		Consumer<Entry> addToNewHistogram = (Entry e) -> newHistogram.addEntry(e);
+		this.forEach(addToNewHistogram);
+		
+		// Replace the old with the new
+		Entry[] newArray = newHistogram.getHistogram();
+		this.histogram = new Entry[newArray.length];
+		
+		System.arraycopy(newArray,0,this.histogram,0,newArray.length);
+		numberOfEntries = newHistogram.getNumberOfEntries();
+	}
+	
+	private Entry[] getHistogram(){
+		return histogram;
+	}
+
 	
 	public Entry contains(String key){
 		int eHash = key.toLowerCase().hashCode();
@@ -89,14 +110,41 @@ public class Histogram {
 		return current;
 	}
 	
-	public int numberOfEntries(){
+	public int getNumberOfEntries(){
 		return numberOfEntries;
 	}
+	
+	public void printOut(){
+		Consumer<Entry> printEntry = (Entry e) -> System.out.println("Key: " + e.getKey() + " Count: " + e.getCount()); 
+		this.forEach(printEntry);
+	}
 
-	/*  
-	*	SOME WAY TO ITERATE THROUGH THE HISTOGRAM
-	* 	DECIDED forEach
-	*/
+	public void forEach(Consumer<Entry> action){
+		for(int i = 0; i<histogram.length; i++ ){
+			Entry current = histogram[i];
+			while(current != null){
+				Entry nextNode = current.next;
+				action.accept(current);
+				current = nextNode;
+			} 
+		}
+	}
 	
+	public Double reduce(Double initial , BiFunction<Double,Entry,Double> action){
+		Double result = initial;
+		
+		for(int i = 0; i<histogram.length; i++ ){
+			Entry current = histogram[i];
+			while(current != null){
+				result = action.apply(result,current);
+				current = current.next;
+			} 
+		}
+		
+		return result;
+	}
 	
+	public URL getURL(){
+		return url;
+	}
 }
